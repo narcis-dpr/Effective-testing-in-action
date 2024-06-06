2e487ccd61345c46edb1598e1f1ddc0e5f21b9c5
narcis <nargesdeypir@gmail.com>
2024-06-06 20:15:10 +0200

    effective and systematic software testing -- different values we want to pass

--- effectiveandsystematicetest/src/test/java/com/example/effectiveT/src/main/java/PlanningPokerTest.java
+++ effectiveandsystematicetest/src/test/java/com/example/effectiveT/src/main/java/PlanningPokerTest.java
@@ -1,6 +1,7 @@
 package com.example.effectiveT.src.main.java;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 import org.junit.Test;
 
@@ -30,5 +31,31 @@ public class PlanningPokerTest {
             List<Estimate> list = Arrays.asList(new Estimate("Eleanor", 1));
             new PlanningPoker().identifyExtremes(list);
         }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void twoEstimates() {
+        List<Estimate> list = Arrays.asList(
+                new Estimate("Mauricio", 10),
+                new Estimate("Frank", 5)
+        );
+        List<String> devs = new PlanningPoker().identifyExtremes(list);
+
+        assertThat(devs).containsExactlyInAnyOrder("Mauricio", "Frank");
+    }
+
+    @Test
+    public void manyEstimates() {
+        List<Estimate> list = Arrays.asList(
+                new Estimate("Mauricio", 10),
+                new Estimate("Arie", 5),
+                new Estimate("Andy", 10),
+                new Estimate("Frank", 7),
+                new Estimate("Annibale", 5)
+        );
+
+        List<String> devs = new PlanningPoker().identifyExtremes(list);
+
+        assertThat(devs).containsExactlyInAnyOrder("Mauricio", "Arie");
     }
 }
