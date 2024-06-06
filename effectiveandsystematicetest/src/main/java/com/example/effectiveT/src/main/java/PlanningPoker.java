a20626958e1ccbc81a744b81dc659fd383e6e27e
narcis <nargesdeypir@gmail.com>
2024-06-06 19:59:33 +0200

    effective and systematic software testing -- poker planning different functionalities

---
+++ effectiveandsystematicetest/src/main/java/com/example/effectiveT/src/main/java/PlanningPoker.java
@@ -0,0 +1,40 @@
+package com.example.effectiveT.src.main.java;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class PlanningPoker {
+
+    public List<String> identifyExtremes(List<Estimate> estimates) {
+
+        if(estimates == null) {
+            throw new IllegalArgumentException("Estimates can't be null");
+        }
+        if(estimates.size() <= 1) {
+            throw new IllegalArgumentException("There has to be more than 1 estimate in the list");
+        }
+
+        Estimate lowestEstimate = null;
+        Estimate highestEstimate = null;
+
+        for(Estimate estimate: estimates) {
+            if(highestEstimate == null ||
+                    estimate.getEstimate() > highestEstimate.getEstimate()) {
+                highestEstimate = estimate;
+            }
+            if(lowestEstimate == null ||
+                    estimate.getEstimate() < lowestEstimate.getEstimate()) {
+                lowestEstimate = estimate;
+            }
+        }
+
+        if(lowestEstimate.equals(highestEstimate))
+            return Collections.emptyList();
+
+        return Arrays.asList(
+                lowestEstimate.getDeveloper(),
+                highestEstimate.getDeveloper()
+        );
+    }
+}
---
+++ effectiveandsystematicetest/src/main/java/com/example/effectiveT/src/main/java/PlanningPokerByJohn.java
@@ -0,0 +1,28 @@
+package com.example.effectiveT.src.main.java;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PlanningPokerByJohn {
+    public List<String> identifyExtremes(List<Estimate> estimates) {
+
+        Estimate lowestEstimate = null;
+        Estimate highestEstimate = null;
+
+        for(Estimate estimate: estimates) {
+            if(highestEstimate == null ||
+                    estimate.getEstimate() > highestEstimate.getEstimate()) {
+                highestEstimate = estimate;
+            }
+            else if(lowestEstimate == null ||
+                    estimate.getEstimate() < lowestEstimate.getEstimate()) {
+                lowestEstimate = estimate;
+            }
+        }
+
+        return Arrays.asList(
+                lowestEstimate.getDeveloper(),
+                highestEstimate.getDeveloper()
+        );
+    }
+}
