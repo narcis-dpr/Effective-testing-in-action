d9c0b6318c53e6c6af0613a8deee2ed35373a3b4
narcis <nargesdeypir@gmail.com>
2024-06-09 10:31:32 +0200

    specification-based testing -- shopping cart : shopping cart

---
+++ specification-basedtest/src/main/java/com/example/specification_based/src/main/java/ShoppingCart.java
@@ -0,0 +1,20 @@
+package com.example.specification_based.src.main.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCart {
+    private List<CartItem> items = new ArrayList<CartItem>();
+
+    public void add(CartItem item) {
+        this.items.add(item);
+    }
+    public double totalPrice() {
+        double totalPrice = 0;
+
+        for (CartItem item : items) {
+            totalPrice += item.getUnitPrice() * item.getQuantity();
+        }
+        return totalPrice;
+    }
+}
