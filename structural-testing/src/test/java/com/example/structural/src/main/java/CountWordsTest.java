7fd4e4219852d51295cb517e87e09ca491f26cfc
narcis <nargesdeypir@gmail.com>
2024-06-10 07:49:49 +0200

    structural testing and code coverage -- Count Words : basic tests

---
+++ structural-testing/src/test/java/com/example/structural/src/main/java/CountWordsTest.java
@@ -0,0 +1,22 @@
+package com.example.structural.src.main.java;
+
+
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.Test;
+
+public class CountWordsTest {
+    @Test
+    public void twoWordsEndingWithS() {
+        int words = new CountWords().count("dogs cats");
+        assertThat(words).isEqualTo(2);
+    }
+
+    @Test
+    public void noWordsAtAll() {
+        int words = new CountWords().count("dog cat");
+        assertThat(words).isEqualTo(0);
+    }
+
+}
