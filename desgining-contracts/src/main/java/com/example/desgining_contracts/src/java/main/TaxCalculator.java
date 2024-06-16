42d8e3d36cf7cca1442c61fdb218f7431c6c9f99
narcis-dpr <nargesdeypir@gmail.com>
2024-06-16 11:39:26 +0330

    designing contract -- tax calculator : replace simple if with assert function

--- desgining-contracts/src/main/java/com/example/desgining_contracts/src/java/main/TaxCalculator.java
+++ desgining-contracts/src/main/java/com/example/desgining_contracts/src/java/main/TaxCalculator.java
@@ -5,10 +5,14 @@ class TaxCalculator {
         if (value < 0) {
             throw new RuntimeException("value cannot be negative");
         }
+        // replace above if with assert :
+        assert value >= 0 : "value cannot be negative";
         double taxValue = 0;
         if (taxValue < 0) {
             throw new RuntimeException("calculated tax value cannot be negative");
         }
+        //replace this if with assert :
+        assert value >= 0 : "calculated tax value cannot be negative";
         return taxValue;
     }
 }
